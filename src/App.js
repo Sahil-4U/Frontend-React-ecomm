620d9507fc3db9a6454c047058604b968a2d9a53
Sahil <ss6611786@gmail.com>
2023-02-06 14:38:15 +0530

    third_class

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import Contact from "./Contact";
 import SingleProduct from "./SingleProduct";
 import Cart from "./Cart";
 import Errorpage from "./Errorpage";
+import { ThemeProvider } from "styled-components";
+import {GlobalStyle} from "./GlobalStyle";
 
 const App = () => {
+  const theme={
+    
+  }
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<Errorpage/>}/>
-        <Route path="/singleproduct/:id" element={<SingleProduct />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <GlobalStyle/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Errorpage />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
